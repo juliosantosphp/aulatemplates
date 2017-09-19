361986e22ab941ec6916795bd4457d0c52639d23
cliente <cliente@192.168.1.206>
2017-09-19 11:42:39 -0300

    este é meu terceiro commit

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
-<?php
+
+
+<html>
+  <?php
 
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+?>
+    <head>
+        <title>aula template</title>
+    </head>
+</html>
 
